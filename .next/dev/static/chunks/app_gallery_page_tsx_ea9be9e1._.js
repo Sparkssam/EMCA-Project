d1e197edd1203d45414b976f61504b67b2ffee57
Derick Gabriel Mhidze <derickmhidze10@gmail.com>
2025-11-15 02:11:07 +0300

    Refactor code structure for improved readability and maintainability

--- .next/dev/static/chunks/app_gallery_page_tsx_ea9be9e1._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_a62874fb._.js",
-  "static/chunks/components_gallery_gallery-grid_tsx_05d7aa01._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_stories_[id]_page_tsx_ea9be9e1._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_532467a2._.js",
-  "static/chunks/app_stories_[id]_page_tsx_4f84b585._.js"
-],
-    source: "dynamic"
-});
