93de4dbd32d65bd4e424bfb03bc9b2f4a585f536
Derick Gabriel Mhidze <derickmhidze10@gmail.com>
2025-11-16 04:37:24 +0300

    feat: add new admin routes for projects and reviews
    - Updated AppRoutes to include "/admin/projects" and "/admin/reviews".
    - Added type validation for the new routes in the validator.ts file.

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -54,6 +54,24 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
+// Validate ../../../app/admin/projects/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/admin/projects">> = Specific
+  const handler = {} as typeof import("../../../app/admin/projects/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
+// Validate ../../../app/admin/reviews/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/admin/reviews">> = Specific
+  const handler = {} as typeof import("../../../app/admin/reviews/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../../app/admin/volunteers/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/admin/volunteers">> = Specific
