7fb5329f7e46dc949f8409b6638890a23244b708
Derick Gabriel Mhidze <derickmhidze10@gmail.com>
2025-11-25 01:48:18 +0300

    Remove .next build artifacts and update events validation for upcoming status

--- .next/dev/static/chunks/app_layout_tsx_469ed6a6._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__081ccb3a._.css",
-  "static/chunks/_1847b835._.js",
-  "static/chunks/node_modules_706745b7._.js"
-],
-    source: "dynamic"
-});
